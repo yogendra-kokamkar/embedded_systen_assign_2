5d10c834bfeb6c32464df03f8de6aa033dcc7b01
Chandra Kiran Saladi <kiranck18@gmail.com>
2018-06-21 00:09:42 -0500

    All *.* files generation missing

--- src/sources.mk
+++ src/sources.mk
@@ -10,8 +10,26 @@
 #*****************************************************************************
 
 # Add your Source files to this variable
-SOURCES =
+
+ifeq ($(PLATFORM),MSP432)
+	SOURCES = interrupts_msp432p401r_gcc.c \
+			   ./main.c \
+			   ./memory.c \
+			   ./startup_msp432p401r_gcc.c \
+			   ./system_msp432p401r.c
+else
+	SOURCES = ./main.c \
+			  ./memory.c \
+
+	endif
 
 # Add your include paths to this variable
-INCLUDES = 
+# Add your include paths to this variable
+ifeq ($(PLATFORM),MSP432)
+	INCLUDES = -I../include/CMSIS \
+			   -I../include/common \
+			   -I../include/msp432 
+else
+	INCLUDES = -I../include/common	
 
+endif
